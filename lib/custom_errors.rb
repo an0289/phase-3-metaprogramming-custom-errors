6f7ccd2a77057a0bba4ba895b0e1656ba98888ed
Alexandra <alexandraneff@live.com>
2023-01-09 20:45:40 -0600

    commit tree 580ec6cb2d6ae240632b4d78a1e39e483d884d37

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -6,9 +6,23 @@ class Person
   end
 
   def get_married(person)
+    if person.is_a?(Person)
     self.partner = person
     person.partner = self
+    else 
+      begin 
+      raise PartnerError 
+      rescue PartnerError => error 
+        puts error.message 
+      end 
+    end 
   end
+
+  class PartnerError < StandardError
+    def message 
+      "you must give the get_marries method an agrument of an instance of the person class!"
+    end 
+  end 
 
 end
 
